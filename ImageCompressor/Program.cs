5cac75e26d02b14452df06b62477d237f70c3b51
kostyanf14 <kostyanf14@K29-USER9>
2019-06-16 14:04:20 +0300

    Added log to file

--- ImageCompressor/Program.cs
+++ ImageCompressor/Program.cs
@@ -12,6 +12,15 @@ namespace ImageCompressor
     {
         const string SourceDirectory = "source";
         const string ResultDirectory = "converted";
+        const string LogFile = "log.log";
+
+        static StreamWriter logWriter;
+        public static void WriteLog(string format,  params object[] args)
+        {
+            var msg = string.Format(format, args);
+            logWriter.WriteLine(msg);
+            Console.WriteLine(msg);
+        }
 
         public static Bitmap GetResizedBitmap(Image image)
         {
@@ -73,14 +82,14 @@ namespace ImageCompressor
                         CompressJpeg(source, dest);
                         return true;
                     default:
-                        Console.WriteLine("Unknown extention: {0}. Can't compress {1}.", ext, source);
+                        WriteLog("Unknown extention: {0}. Can't compress {1}.", ext, source);
                         File.Copy(source, dest);
                         return false;
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Can't compress {0}. Exception: {1}.",
+                WriteLog("Can't compress {0}. Exception: {1}.",
                     source, ex.Message);
 
                 return false;
@@ -94,6 +103,9 @@ namespace ImageCompressor
                 Directory.CreateDirectory(SourceDirectory);
             if (!Directory.Exists(ResultDirectory))
                 Directory.CreateDirectory(ResultDirectory);
+
+            logWriter = new StreamWriter(LogFile);
+            logWriter.WriteLine("Started at {0}", DateTime.Now.ToString());
 
             var sourceFiles = Directory.GetFiles(SourceDirectory, "*", SearchOption.AllDirectories);
 
@@ -111,12 +123,12 @@ namespace ImageCompressor
                     var res = Compress(file, dest);
 
                     processed++;
-                    Console.WriteLine("{0} of {1}: {2} - {3}",
+                    WriteLog("{0} of {1}: {2} - {3}",
                         processed, sourceFiles.Length, file, res ? "ok" : "fail");
                 }
             );
 
-            Console.WriteLine("Job done!");
+            WriteLog("Job done!");
             Console.ReadLine();
         }
     }
